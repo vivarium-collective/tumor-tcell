66a3a6cc51227f5dae9b9f09cbb9c5485937b9ac
Eran Agmon <agmon.eran@gmail.com>
2020-11-21 11:12:21 -0800

    plot diffusion field test output

--- tumor_tcell/processes/fields.py
+++ tumor_tcell/processes/fields.py
@@ -4,17 +4,26 @@ Diffusion Field
 ===============
 '''
 
+import os
+
 import numpy as np
 from scipy import constants
 from scipy.ndimage import convolve
 
 from vivarium.core.process import Process
-from vivarium.library.units import units
+from vivarium.core.composition import simulate_process
+from vivarium.library.units import units, remove_units
 from vivarium_cell.library.lattice_utils import (
     count_to_concentration,
     get_bin_site,
     get_bin_volume,
 )
+
+# plotting
+from vivarium_cell.plots.multibody_physics import plot_snapshots
+
+# directories
+from tumor_tcell import PROCESS_OUT_DIR
 
 
 NAME = 'fields'
@@ -35,20 +44,21 @@ class Fields(Process):
     name = NAME
     defaults = {
         'time_step': 1,
-        'molecules': ['molecule'],
+        'molecules': ['IFNg'],
         'initial_state': {},
         'n_bins': [10, 10],
         'bounds': [10 * units.um, 10 * units.um],
         'depth': 3000.0,  # um
-        'diffusion': 5e-1,
+        'default_diffusion_dt': 0.01,
+        'default_diffusion_rate': 5e-1,
         'gradient': {},
-
-        #TODO - Add the diffusion and degradation of IFNg
-        'IFNg_diffusion_coeff': 1.25 * 10**-3, #cm^2/day #(Liao, 2014)
-        'IFNg_degradation_coeff': 2.16,  # 1/day #(Liao, 2014)
+        'diffusion': {
+            'IFNg': 1.25e-3,  # cm^2/day #(Liao, 2014)
+        },
+        'degradation': {
+            'IFNg': 2.16,  # 1/day #(Liao, 2014)
+        },
         #If we want to add recycling - 100-1000 molecules/cell/min #(Zhou, 2018)
-
-
     }
 
     def __init__(self, parameters=None):
@@ -62,8 +72,9 @@ class Fields(Process):
         self.bounds = [b.to(LENGTH_UNIT).magnitude for b in self.parameters['bounds']]
         self.depth = self.parameters['depth']
 
-        # diffusion
-        diffusion = self.parameters['diffusion']
+        # get diffusion rates
+        # TODO (ERAN) -- make molecule-specific diffusion rate dictionary
+        diffusion_rate = self.parameters['default_diffusion_rate']
         bins_x = self.n_bins[0]
         bins_y = self.n_bins[1]
         length_x = self.bounds[0]
@@ -71,9 +82,11 @@ class Fields(Process):
         dx = length_x / bins_x
         dy = length_y / bins_y
         dx2 = dx * dy
-        self.diffusion = diffusion / dx2
-        self.diffusion_dt = 0.01
-        # self.diffusion_dt = 0.5 * dx ** 2 * dy ** 2 / (2 * self.diffusion * (dx ** 2 + dy ** 2))
+        self.diffusion = diffusion_rate / dx2
+
+        # get diffusion timestep
+        diffusion_dt = 0.5 * dx ** 2 * dy ** 2 / (2 * diffusion_rate * (dx ** 2 + dy ** 2))
+        self.diffusion_dt = min(diffusion_dt, self.parameters['default_diffusion_dt'])
 
         # volume, to convert between counts and concentration
         self.bin_volume = get_bin_volume(self.n_bins, self.bounds, self.depth)
@@ -82,20 +95,22 @@ class Fields(Process):
         if config is None:
             config = {}
         if 'random' in config:
-            return {
-                'fields': {
-                    field: self.random_field()
-                    for field in self.parameters['molecules']}}
+            max = config.get('random', 1)
+            fields = {
+                field: max * self.random_field()
+                for field in self.parameters['molecules']}
         elif 'uniform' in config:
-            return {
-                'fields': {
-                    field: config['uniform']*self.ones_field()
-                    for field in self.parameters['molecules']}}
+            fields = {
+                field: config['uniform'] * self.ones_field()
+                for field in self.parameters['molecules']}
         else:
-            return {
-                'fields': {
-                    field: self.ones_field()
-                    for field in self.parameters['molecules']}}
+            fields = {
+                field: self.ones_field()
+                for field in self.parameters['molecules']}
+        return {
+            'fields': fields,
+            'cells': {},
+        }
 
     def ports_schema(self):
         local_concentration_schema = {
@@ -168,11 +183,6 @@ class Fields(Process):
 
         return update
 
-    def count_to_concentration(self, count):
-        return count_to_concentration(
-            count * units.count, self.bin_volume * units.L
-        ).to(units.mmol / units.L).magnitude
-
     def get_bin_site(self, location):
         return get_bin_site(
             [l.to(LENGTH_UNIT).magnitude for l in location],
@@ -238,3 +248,47 @@ class Fields(Process):
     def degrade(self, fields, timestep):
         import ipdb; ipdb.set_trace()
         return {}
+
+def test_fields(config={}):
+
+    # initialize process
+    fields = Fields(config)
+
+    # get initial state from process
+    initial_state = fields.initial_state({'random': 5.0})
+
+    # run experiment
+    settings = {
+        'return_raw_data': True,
+        'initial_state': initial_state,
+        'total_time': 10,
+        'timestep': 1}
+    return simulate_process(fields, settings)
+
+def plot_fields(data, config, out_dir='out', filename='fields'):
+    fields = {
+        time: time_data['fields']
+        for time, time_data in data.items()}
+    snapshots_data = {
+        'fields': fields,
+        'config': config}
+    plot_config = {
+        'out_dir': out_dir,
+        'filename': filename}
+    plot_snapshots(snapshots_data, plot_config)
+
+
+if __name__ == '__main__':
+    out_dir = os.path.join(PROCESS_OUT_DIR, NAME)
+    os.makedirs(out_dir, exist_ok=True)
+
+    config = {
+        'n_bins': [10, 10],
+        'bounds': [10 * units.um, 10 * units.um]}
+    data = test_fields(config)
+
+    # plot
+    plot_fields(
+        data,
+        remove_units(config),
+        out_dir, 'test_field')
