e0c35f256593e86dd4d6b3a42889ea61779928d4
johnhickey <johnhickey22@gmail.com>
2020-11-28 16:56:49 -0800

    made some small changes to the experimental conditions and size of the cells and commented out print statement in process

--- tumor_tcell/processes/t_cell.py
+++ tumor_tcell/processes/t_cell.py
@@ -45,7 +45,7 @@ class TCellProcess(Process):
     name = NAME
     defaults = {
         'time_step': TIMESTEP,
-        'diameter': 10 * units.um,  # 0.01 * units.mm,
+        'diameter': 5 * units.um,  # 0.01 * units.mm,
         'initial_PD1n': 0.8,
         'transition_PD1n_to_PD1p_10days': 0.95,  # nearly 95% by 10 days (Wherry 2007)
         'cellstate_transition_time': 259200,  # extended contact with tumor cells expressing MHCI for more
