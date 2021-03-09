04d72d3e726fad280c0d3c612fce8700b43fe7e1
johnhickey <johnhickey22@gmail.com>
2021-03-08 16:19:32 -0800

    v0.0.21

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='tumor-tcell',
-    version='0.0.20',
+    version='0.0.21',
     packages=[
         'tumor_tcell',
         'tumor_tcell.composites',
