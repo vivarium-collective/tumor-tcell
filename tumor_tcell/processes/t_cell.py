461f5eb3a6b3ffd2fe507dd48ab68163b91ebbb4
Eran Agmon <agmon.eran@gmail.com>
2021-05-10 14:54:27 -0700

    get tcell agent test working with a timeline

--- tumor_tcell/processes/t_cell.py
+++ tumor_tcell/processes/t_cell.py
@@ -22,10 +22,10 @@ NAME = 'T_cell'
 TIMESTEP = 60  # seconds
 CONCENTRATION_UNIT = 1
 
-def assymetric_division(mother_value, states):
+def assymetric_division(mother_value, **args):
     return [mother_value+1, mother_value]
 
-def set_velocity_default(mother_value, states):
+def set_velocity_default(mother_value, **args):
     return [10.0 * units.um/units.min, 10.0 * units.um/units.min]
 
 def get_probability_timestep(probability_parameter, timescale, timestep):
@@ -143,19 +143,16 @@ class TCellProcess(Process):
                     '_updater': 'set'},
                 'divide': {
                     '_default': False,
-                    #'_emit': False, #true for monitoring behavior in process
                     '_updater': 'set'},
                 'PD1n_divide_count': {
                     '_default': 0,
-                    #'_emit': False, #true for monitoring behavior in process
                     '_divider': {
                         'divider': assymetric_division,
-                        'topology': {}
+                        'config': {}
                     },
                     '_updater': 'accumulate'},
                 'PD1p_divide_count': {
                     '_default': 0,
-                    #'_emit': False, #true for monitoring behavior in process
                     '_updater': 'accumulate'}
             },
             'internal': {
@@ -166,12 +163,10 @@ class TCellProcess(Process):
                 },
                 'cell_state_count': {
                     '_default': 0,
-                    #'_emit': False, #true for monitoring behavior in process
                     '_updater': 'accumulate'
                 },
                 'refractory_count': {
                     '_default': 0,
-                    #'_emit': True, #true for monitoring behavior in process
                     '_updater': 'accumulate'
                 },
                 'total_cytotoxic_packets': {
@@ -207,14 +202,13 @@ class TCellProcess(Process):
                     '_updater': 'set',
                     '_divider': {
                         'divider': set_velocity_default,
-                        'topology': {}
-                                 },
+                        # 'topology': {}
+                         },
                     '_emit': True,
                 },
                 'exchange': {
                     'IFNg': {
                         '_default': 0,  # counts
-                        #'_emit': False, #true for monitoring behavior in process
                         '_updater': 'accumulate',
                         '_divider': 'split',
                     }},
@@ -597,6 +591,61 @@ def test_single_t_cell(
     plot_simulation_output(timeseries, plot_settings, out_dir, NAME + '_single')
 
 
+TCELL_OVERRIDE = {
+    '_schema': {
+        'globals': {
+            'death': {
+                '_emit': False
+            }
+        },
+         'internal': {
+            'cell_state_count': {
+                '_emit': False
+            },
+             'cell_state': {
+                 '_emit': False
+             },
+            'refractory_count': {
+                '_emit': True
+            },
+             'total_cytotoxic_packets': {
+                 '_emit': True
+             },
+             'TCR_timer': {
+                 '_emit': True
+             },
+             'velocity_timer': {
+                 '_emit': False
+             },
+        },
+        'neighbors': {
+            'present': {
+                'PD1': {
+                    '_emit': True
+                },
+            },
+            'accept': {
+                'PDL1': {
+                    '_emit': True
+                },
+                'MHCI': {
+                    '_emit': True
+                },
+            },
+        },
+        'boundary': {
+            'velocity': {
+                '_emit': False
+            },
+            'external': {
+                'IFNg': {
+                    '_emit': False
+                },
+            }
+        },
+    }
+}
+
 def test_batch_t_cell(
     total_time=43200,
     time_step=TIMESTEP,
@@ -604,64 +653,9 @@ def test_batch_t_cell(
     timeline=None,
     out_dir='out'):
 
-    override_schema = {
-        '_schema': {
-            'globals': {
-                'death': {
-                    '_emit': False
-                }
-            },
-             'internal': {
-                'cell_state_count': {
-                    '_emit': False
-                },
-                 'cell_state': {
-                     '_emit': False
-                 },
-                'refractory_count': {
-                    '_emit': True
-                },
-                 'total_cytotoxic_packets': {
-                     '_emit': True
-                 },
-                 'TCR_timer': {
-                     '_emit': True
-                 },
-                 'velocity_timer': {
-                     '_emit': False
-                 },
-            },
-            'neighbors': {
-                'present': {
-                    'PD1': {
-                        '_emit': True
-                    },
-                },
-                'accept': {
-                    'PDL1': {
-                        '_emit': True
-                    },
-                    'MHCI': {
-                        '_emit': True
-                    },
-                },
-            },
-            'boundary': {
-                'velocity': {
-                    '_emit': False
-                },
-                'external': {
-                    'IFNg': {
-                        '_emit': False
-                    },
-                }
-            },
-        }
-    }
-
     combined_raw_data = {}
     for single_idx in range(batch_size):
-        t_cell_process = TCellProcess(override_schema)
+        t_cell_process = TCellProcess(TCELL_OVERRIDE)
         if timeline is not None:
             sim_settings = {
                 'timeline': {
